1151ecf25e7adac0de239dd93708ca6227644ec8
Maram <maramkhaled141@gmail.com>
2023-11-04 00:09:27 +0200

    added getMinimum function

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include<math.h>
 using namespace std;
 long long getSum(vector<long long> arr)
 {
@@ -8,10 +9,19 @@ long long getSum(vector<long long> arr)
 
     return sum;
 }
+long long getMinimum(vector<long long> arr)
+{
+    long long minimumValue = arr[0];
+    for(auto &curElement : arr)
+        minimumValue = min(minimumValue, curElement);
+
+    return minimumValue;
+}
 int main()
 {
     vector<long long> arr;
     for(int i = 1;i <= 10;++i) 
         arr.push_back(i);
     cout << getSum(arr) << endl;
+    cout << getMinimum(arr) << endl;
 }
